f2d21f4d1a322b713606016b69429126be630cc8
sauli <sauli@favorPC>
2024-04-15 00:48:09 +0300

    Мой первый коммит!

---
+++ src/main/java/com/mycompany/practical2_2/Practical2_2.java
@@ -0,0 +1,62 @@
+
+
+package com.mycompany.practical2_2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class Practical2_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Practical 2.2 Saulin Ivan RIBO-01-22 Variant 4");
+        System.out.println("Start program!");
+        String server = "https://android-for-students.ru";
+        String serverPath = "/materials/practical/hello.php";
+        HashMap<String,String> map = new HashMap();
+        map.put("name","Saulin");
+        map.put("group","RIBO-01-22");
+        HTTPRunnable httpRunnable = new HTTPRunnable(server+serverPath,map);
+        Thread th = new Thread(httpRunnable);
+        th.start();
+        try{
+            th.join();
+        }catch(InterruptedException ex){
+            
+        }finally{
+            JSONObject jSONObject = new JSONObject(httpRunnable.getResponseBody());
+            int result = jSONObject.getInt("result_code");
+            System.out.println("Result: " + result);
+            System.out.println("Type: " + jSONObject.getString("message_type"));
+            System.out.println("Text: " + jSONObject.getString("message_text"));
+            switch(result){
+                case 1:
+                    JSONArray jSONArray = jSONObject.getJSONArray("task_list");
+                    System.out.println("Task list: ");
+                    for(int i = 0;i<jSONArray.length();i++){
+                        System.out.println((i+1) + ") " + jSONArray.get(i));
+                    }
+                    break;
+                case 0:
+                    System.out.println("Server returned an error.");
+                    String errorText = jSONObject.getString("message_text");
+                    System.out.println("Error message: " + errorText);
+
+                    if (jSONObject.has("error_details")) {
+                        String errorDetails = jSONObject.getString("error_details");
+                        System.out.println("Error details: " + errorDetails);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        
+        
+        
+    }
+}
